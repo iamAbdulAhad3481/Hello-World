6ed8d7e2e432e8987b2f24b7b9c75bad532124aa
Ahad <ahad3481@gmail.com>
2019-12-16 15:07:51 +0500

    "Adding Space"
    :wq

---
+++ src/Components/LifeCycleA.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class LifeCycleA extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+        
+    }
+    
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LifeCycleA
