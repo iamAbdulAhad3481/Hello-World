d9617941be81394d4cd8210a370617081c09d600
Ahad <ahad3481@gmail.com>
2019-12-16 15:30:06 +0500

    Adding LifeCycle Methods in LifeCycleA Component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import EventCalender from './Components/EventCalender';
 import MainRoute from './RouteComponents/MainRoute';
+import LifeCycleA from './Components/LifeCycleA';
 
 function App() {
   return (
     <div className="App">
     
-       <MainRoute/>
+       {/* <MainRoute/> */}
         {/* <EventCalender/> */}
+        <LifeCycleA/>
     
     </div>
   );
--- src/Components/LifeCycleA.js
+++ src/Components/LifeCycleA.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 
 export class LifeCycleA extends Component {
-
     constructor(props) {
         super(props)
     
         this.state = {
              
         }
+        console.log('LifeCycle A Constructor')
+    }
+    static getDerivedStateFromProps(props){
+        console.log('LifeCycle A getDerivedStateFromProps')
+        return(
+            null
+        )
+    }
+    componentDidMount()
+    {
+        console.log('LifeCycle A componentDidMount')
         
     }
-    
-
 
     render() {
+        console.log('LifeCycle A render')
+
         return (
             <div>
+                LifeCycleA 
                 
             </div>
         )
