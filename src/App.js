73070d18d5a0ac96efb521eb8b057240785e0c44
Ahad <ahad3481@gmail.com>
2019-12-17 14:39:23 +0500

    Adding Components and doing crud operation through axios in them

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 import EventCalender from './Components/EventCalender';
 import MainRoute from './RouteComponents/MainRoute';
 import LifeCycleA from './Components/LifeCycleA';
+import Main from './Axios Requests/Main';
+import EditUserModal from './Axios Requests/EditUserModal';
 
 function App() {
   return (
     <div className="App">
-    
-       {/* <MainRoute/> */}
-        {/* <EventCalender/> */}
-        <LifeCycleA/>
-    
+
+      {/* <MainRoute/> */}
+      {/* <EventCalender/> */}
+      {/* <LifeCycleA/> */}
+      <Main/>
+
     </div>
   );
 }
